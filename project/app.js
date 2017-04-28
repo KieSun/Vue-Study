c4fd37a6d38faed59ef9696741c6549e3c4c55ea
俞诚恺 <zx597813039@gmail.com>
2017-04-28 16:33:41 +0800

    完成登录注册登出功能

--- project/app.js
+++ project/app.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
+import AV from 'leancloud-storage'
 
-
+var APP_ID = 'EGzdmuxs5aAyzQa9XjqGF8dT-gzGzoHsz'
+var APP_KEY = 'tBJxlXJeuG8MAWcdWRbdTkIx'
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
 
 var app = new Vue({
     el: '#app',
@@ -8,6 +14,12 @@ var app = new Vue({
         return {
             newTodo: '',
             todoList: [],
+            actionType: 'signUp',
+            formData: {
+                username: '',
+                password: ''
+            },
+            currentUser: null
         }
     },
     created: function() {
@@ -17,6 +29,8 @@ var app = new Vue({
             window.localStorage.setItem('myTodos', dataString)
             let newTodo = JSON.stringify(this.newTodo)
             window.localStorage.setItem('newTodo', newTodo)
+            let oldUser = JSON.stringify(this.currentUser)
+            window.localStorage.setItem('user', oldUser)
         }
 
         let oldDataString = window.localStorage.getItem('myTodos')
@@ -26,6 +40,8 @@ var app = new Vue({
         let oldnewTodo = window.localStorage.getItem('newTodo')
         let oldnewData = JSON.parse(oldnewTodo)
         this.newTodo = oldnewData || ''
+        let user = window.localStorage.getItem('user')
+        this.currentUser = JSON.parse(user) || ''
     },
     methods: {
         addTodo: function() {
@@ -40,8 +56,29 @@ var app = new Vue({
         },
         removeTodo: function(todo) {
             let index = this.todoList.indexOf(todo)
-            this.todoList.pop()
             this.todoList.splice(index, 1)
+        },
+        signUp: function() {
+            console.log('signUp');
+            var user = new AV.User()
+            user.setUsername(this.formData.username)
+            user.setPassword(this.formData.password)
+            user.signUp().then( () => {
+                this.currentUser = this.getCurrentUser()
+            })
+        },
+        login: function() {
+            AV.User.logIn(this.formData.username, this.formData.password)
+                .then( () => {
+                    this.currentUser = this.getCurrentUser()
+                })
+        },
+        getCurrentUser: function() {
+            let { id, createdAt, attributes: {username} } = AV.User.current()
+            return {id, createdAt, username}
+        },
+        loginOut: function() {
+            this.currentUser = null
         }
-    }
+      }
 })
