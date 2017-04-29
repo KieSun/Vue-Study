208863b4329f728a404e10357c21908f30f61f3e
yuchengkai <zx597813039@gmail.com>
2017-04-29 16:45:56 +0800

    Finish todo

--- project/app.js
+++ project/app.js
@@ -4,13 +4,13 @@ import AV from 'leancloud-storage'
 var APP_ID = 'EGzdmuxs5aAyzQa9XjqGF8dT-gzGzoHsz'
 var APP_KEY = 'tBJxlXJeuG8MAWcdWRbdTkIx'
 AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
+    appId: APP_ID,
+    appKey: APP_KEY
 });
 
 var app = new Vue({
     el: '#app',
-    data () {
+    data() {
         return {
             newTodo: '',
             todoList: [],
@@ -22,63 +22,115 @@ var app = new Vue({
             currentUser: null
         }
     },
-    created: function() {
-        // 窗口即将被卸载
-        window.onbeforeunload = () => {
-            let dataString = JSON.stringify(this.todoList)
-            window.localStorage.setItem('myTodos', dataString)
-            let newTodo = JSON.stringify(this.newTodo)
-            window.localStorage.setItem('newTodo', newTodo)
-            let oldUser = JSON.stringify(this.currentUser)
-            window.localStorage.setItem('user', oldUser)
+    created: function () {
+
+        if (this.getCurrentUser()) {
+            this.currentUser = this.getCurrentUser()
+            this.refreshData()
         }
 
-        let oldDataString = window.localStorage.getItem('myTodos')
-        let oldData = JSON.parse(oldDataString)
-        this.todoList = oldData || []
-
-        let oldnewTodo = window.localStorage.getItem('newTodo')
-        let oldnewData = JSON.parse(oldnewTodo)
-        this.newTodo = oldnewData || ''
-        let user = window.localStorage.getItem('user')
-        this.currentUser = JSON.parse(user) || ''
     },
     methods: {
-        addTodo: function() {
-            if (!this.newTodo) { return }
-            this.todoList.push({
+        addTodo: function () {
+            if (!this.newTodo) {
+                return
+            }
+            let data = {
                 title: this.newTodo,
                 createdAt: new Date(),
                 done: false
-            })
+            }
+            this.todoList.push(data)
             this.newTodo = ''
             console.log(this.todoList);
+            this.saveOrUpdateTodos()
         },
-        removeTodo: function(todo) {
+        removeTodo: function (todo) {
             let index = this.todoList.indexOf(todo)
             this.todoList.splice(index, 1)
+            this.saveOrUpdateTodos()
         },
-        signUp: function() {
+        signUp: function () {
             console.log('signUp');
             var user = new AV.User()
             user.setUsername(this.formData.username)
             user.setPassword(this.formData.password)
-            user.signUp().then( () => {
+            user.signUp().then(() => {
                 this.currentUser = this.getCurrentUser()
             })
         },
-        login: function() {
+        login: function () {
             AV.User.logIn(this.formData.username, this.formData.password)
-                .then( () => {
+                .then(() => {
                     this.currentUser = this.getCurrentUser()
+                    this.refreshData()
                 })
         },
-        getCurrentUser: function() {
-            let { id, createdAt, attributes: {username} } = AV.User.current()
-            return {id, createdAt, username}
+        getCurrentUser: function () {
+            if (!AV.User.current()) {  return }
+            let {
+                id,
+                createdAt,
+                attributes: {
+                    username
+                }
+            } = AV.User.current()
+            return {
+                id,
+                createdAt,
+                username
+            }
         },
-        loginOut: function() {
+        loginOut: function () {
+            AV.User.logOut();
             this.currentUser = null
+        },
+        saveTodos: function () {
+            let dataString = JSON.stringify(this.todoList)
+            var AVTodos = AV.Object.extend('AllTodos')
+            var avTodos = new AVTodos()
+            var acl = new AV.ACL()
+            acl.setWriteAccess(AV.User.current(), true)
+            acl.setReadAccess(AV.User.current(), true)
+            avTodos.setACL(acl)
+            avTodos.set('content', dataString)
+            avTodos.save().then(function (todo) {
+                this.todoList.id = todo.id
+            }, function (error) {})
+        },
+        updateTodos: function () {
+            let dataString = JSON.stringify(this.todoList)
+            // console.log(dataString);
+            let avTodos = AV.Object.createWithoutData('AllTodos', this.todoList.id)
+            avTodos.set('content', dataString)
+            avTodos.save().then(() => {
+                console.log('update success');
+            })
+        },
+        saveOrUpdateTodos: function () {
+            if (this.todoList.id) {
+                this.updateTodos()
+            } else {
+                this.saveTodos()
+            }
+        },
+        finish: function() {
+            this.saveOrUpdateTodos()
+        },
+        refreshData: function() {
+            var query = new AV.Query('AllTodos');
+            query.find().then( (todos) => {
+
+                let avAllTodos = todos[0]
+                let id = avAllTodos.id
+                this.todoList = JSON.parse(avAllTodos.attributes.content)
+                this.todoList.id = id
+                console.log(this.todoList);
+            }).then(function (todos) {
+                // 更新成功
+            }, function (error) {
+                // 异常处理
+            });
         }
-      }
+    }
 })
