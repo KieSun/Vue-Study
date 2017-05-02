faf27393a8f2fa437d8ca7b91bde927067c2b991
yuchengkai <zx597813039@gmail.com>
2017-05-02 08:55:57 +0800

    Resume init

---
+++ resumer/src/components/Topbar.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="topBar">
+        <div class="wrapper">
+            <span class="logo">Resumer</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'topBar'
+    }
+</script>
+
+<style>
+    #topBar {
+        background: #fff;
+        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.25);
+    }
+    #topBar > .wrapper {
+        min-width: 1024px;
+        max-width: 1440px;
+        margin: 0 auto;
+        height: 64px;
+    }
+    #topBar > .wrapper {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    #topBar .logo {
+        font-size: 24px;
+        color: #000;
+        padding: 0 16px;
+    }
+</style>
